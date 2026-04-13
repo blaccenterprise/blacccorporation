6edf38105c3740d74405258c7db343432a5b2237
Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>
2026-04-13 08:26:17 -0700

    Add files via upload
    Signed-off-by: Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>

---
+++ BLACCCORPORATION_Term.js
@@ -0,0 +1,25 @@
+
+         var search_terms = {
+	term1: "Biology",
+	term2: "OneiroNet",
+	term3: "B.L.A.C.C. Ark",
+	term4: "Asclepieion of B.L.A.C.C.",
+	term5: "Language",
+	term6: "Vital Mathematics",
+	term7: "Pharaoh History",
+	term8: "Architecture",
+	term9: "Blacklining",
+	term10: "NFT",
+	term11: "Eye",
+	term12: "Computer Science",
+	term13: "B.L.A.C.C. Connect",
+	term14: "New Nexxus",
+	term15: "Website Inquiry",
+	term16: "B.L.A.C.C. Tele",
+	term17: "Humanities",
+	term18: "Change",
+	term19: "B.L.A.C.C. Guided Pathway",
+	term20: "B.L.A.C.C. Music",
+};
+
+
---
+++ import.js
@@ -0,0 +1,668 @@
+
+        function f(term) {
+
+            let term1 = search_terms.term1;
+
+            let term2 = search_terms.term2;
+
+            let term3 = search_terms.term3;
+
+            let term4 = search_terms.term4;
+
+            let term5 = search_terms.term5;
+
+            let term6 = search_terms.term6;
+
+            let term7 = search_terms.term7;
+
+            let term8 = search_terms.term8;
+
+            let term9 = search_terms.term9;
+
+            let term10 = search_terms.term10;
+
+            let term11 = search_terms.term11;
+
+            let term12 = search_terms.term12;
+
+            let term13 = search_terms.term13;
+
+            let term14 = search_terms.term14;
+
+            let term15 = search_terms.term15;
+
+            let term16 = search_terms.term16;
+
+            let term17 = search_terms.term17;
+
+            let term18 = search_terms.term18;
+
+            let term19 = search_terms.term19;
+
+            let term20 = search_terms.term20;
+/*
+            let term21 = search_terms.term21;
+
+            let term22 = search_terms.term22;
+
+            let term23 = search_terms.term23;
+
+            let term24 = search_terms.term24;
+
+            let term25 = search_terms.term25;
+
+            let term26 = search_terms.term26;
+
+            let term27 = search_terms.term27;
+
+            let term28 = search_terms.term28;
+
+            let term29 = search_terms.term29;
+
+            let term30 = search_terms.term30;
+
+            let term31 = search_terms.term31;
+
+            let term32 = search_terms.term32;
+
+            let term33 = search_terms.term33;
+
+            let term34 = search_terms.term34;
+
+            let term35 = search_terms.term35;
+
+            let term36 = search_terms.term36;
+
+            let term37 = search_terms.term37;
+
+            let term38 = search_terms.term38;
+
+            let term39 = search_terms.term39;
+
+            let term40 = search_terms.term40;
+
+            let term41 = search_terms.term41;
+
+            let term42 = search_terms.term42;
+
+            let term43 = search_terms.term43;
+
+            let term44 = search_terms.term44;
+
+            let term45 = search_terms.term45;
+
+            let term46 = search_terms.term46;
+
+            let term47 = search_terms.term47;
+
+            let term48 = search_terms.term48;
+
+            let term49 = search_terms.term49;
+
+            let term50 = search_terms.term50;
+
+            let term51 = search_terms.term51;
+
+            let term52 = search_terms.term52;
+
+            let term53 = search_terms.term53;
+
+            let term54 = search_terms.term54;
+
+            let term55 = search_terms.term55;
+
+            let term56 = search_terms.term56;
+
+            let term57 = search_terms.term57;
+
+            let term58 = search_terms.term58;
+
+            let term59 = search_terms.term59;
+
+            let term60 = search_terms.term60;
+
+            let term61 = search_terms.term61;
+
+            let term62 = search_terms.term62;
+
+            let term63 = search_terms.term63;
+
+            let term64 = search_terms.term64;
+
+            let term65 = search_terms.term65;
+
+            let term66 = search_terms.term66;
+
+            let term67 = search_terms.term67;
+
+            let term68 = search_terms.term68;
+
+            let term69 = search_terms.term69;
+
+            let term70 = search_terms.term70;
+
+            let term71 = search_terms.term71;
+
+            let term72 = search_terms.term72;
+
+            let term73 = search_terms.term73;
+*/
+
+            let str1 = term1;
+
+            let str2 = term2;
+
+            let str3 = term3;
+
+            let str4 = term4;
+
+            let str5 = term5;
+
+            let str6 = term6;
+
+            let str7 = term7;
+
+            let str8 = term8;
+
+            let str9 = term9;
+
+            let str10 = term10;
+
+            let str11 = term11;
+
+            let str12 = term12;
+
+            let str13 = term13;
+
+            let str14 = term14;
+
+            let str15 = term15;
+
+            let str16 = term16;
+
+            let str17 = term17;
+
+            let str18 = term18;
+
+            let str19 = term19;
+
+            let str20 = term20;
+/*
+            let str21 = term21;
+
+            let str22 = term22;
+
+            let str23 = term23;
+
+            let str24 = term24;
+
+            let str25 = term25;
+
+            let str26 = term26;
+
+            let str27 = term27;
+
+            let str28 = term28;
+
+            let str29 = term29;
+
+            let str30 = term30;
+
+            let str31 = term31;
+
+            let str32 = term32;
+
+            let str33 = term33;
+
+            let str34 = term34;
+
+            let str35 = term35;
+
+            let str36 = term36;
+
+            let str37 = term37;
+
+            let str38 = term38;
+
+            let str39 = term39;
+
+            let str40 = term40;
+
+            let str41 = term41;
+
+            let str42 = term42;
+
+            let str43 = term43;
+
+            let str44 = term44;
+
+            let str45 = term45;
+
+            let str46 = term46;
+
+            let str47 = term47;
+
+            let str48 = term48;
+
+            let str49 = term49;
+
+            let str50 = term50;
+
+            let str51 = term51;
+
+            let str52 = term52;
+
+            let str53 = term53;
+
+            let str54 = term54;
+
+            let str55 = term55;
+
+            let str56 = term56;
+
+            let str57 = term57;
+
+            let str58 = term58;
+
+            let str59 = term59;
+
+            let str60 = term60;
+
+            let str61 = term61;
+
+            let str62 = term62;
+
+            let str63 = term63;
+
+            let str64 = term64;
+
+            let str65 = term65;
+
+            let str66 = term66;
+
+            let str67 = term67;
+
+            let str68 = term68;
+
+            let str69 = term69;
+
+            let str70 = term70;
+
+            let str71 = term71;
+
+            let str72 = term72;
+
+            let str73 = term73;
+*/
+
+            document.getElementById(
+
+                '1').innerHTML = str1;
+
+
+            document.getElementById(
+
+                '2').innerHTML = str2;
+
+
+            document.getElementById(
+
+                '3').innerHTML = str3;
+
+
+            document.getElementById(
+
+                '4').innerHTML = str4;
+
+
+            document.getElementById(
+
+                '5').innerHTML = str5;
+
+
+            document.getElementById(
+
+                '6').innerHTML = str6;
+
+
+            document.getElementById(
+
+                '7').innerHTML = str7;
+
+
+            document.getElementById(
+
+                '8').innerHTML = str8;
+
+
+            document.getElementById(
+
+                '9').innerHTML = str9;
+
+
+            document.getElementById(
+
+                '10').innerHTML = str10;
+
+
+            document.getElementById(
+
+                '11').innerHTML = str11;
+
+
+            document.getElementById(
+
+                '12').innerHTML = str12;
+
+
+            document.getElementById(
+
+                '13').innerHTML = str13;
+
+
+            document.getElementById(
+
+                '14').innerHTML = str14;
+
+
+            document.getElementById(
+
+                '15').innerHTML = str15;
+
+
+            document.getElementById(
+
+                '16').innerHTML = str16;
+
+
+            document.getElementById(
+
+                '17').innerHTML = str17;
+
+
+            document.getElementById(
+
+                '18').innerHTML = str18;
+
+
+            document.getElementById(
+
+                '19').innerHTML = str19;
+
+
+            document.getElementById(
+
+                '20').innerHTML = str20;
+/*
+
+            document.getElementById(
+
+                '21').innerHTML = str21;
+
+
+            document.getElementById(
+
+                '22').innerHTML = str22;
+
+
+            document.getElementById(
+
+                '23').innerHTML = str23;
+
+
+            document.getElementById(
+
+                '24').innerHTML = str24;
+
+
+            document.getElementById(
+
+                '25').innerHTML = str25;
+
+
+            document.getElementById(
+
+                '26').innerHTML = str26;
+
+
+            document.getElementById(
+
+                '27').innerHTML = str27;
+
+
+            document.getElementById(
+
+                '28').innerHTML = str28;
+
+
+            document.getElementById(
+
+                '29').innerHTML = str29;
+
+
+            document.getElementById(
+
+                '30').innerHTML = str30;
+
+
+            document.getElementById(
+
+                '31').innerHTML = str31;
+
+
+            document.getElementById(
+
+                '32').innerHTML = str32;
+
+
+            document.getElementById(
+
+                '33').innerHTML = str33;
+
+
+            document.getElementById(
+
+                '34').innerHTML = str34;
+
+
+            document.getElementById(
+
+                '35').innerHTML = str35;
+
+
+            document.getElementById(
+
+                '36').innerHTML = str36;
+
+
+            document.getElementById(
+
+                '37').innerHTML = str37;
+
+
+            document.getElementById(
+
+                '38').innerHTML = str38;
+
+
+            document.getElementById(
+
+                '39').innerHTML = str39;
+
+
+            document.getElementById(
+
+                '40').innerHTML = str40;
+
+
+            document.getElementById(
+
+                '41').innerHTML = str41;
+
+
+            document.getElementById(
+
+                '42').innerHTML = str42;
+
+
+            document.getElementById(
+
+                '43').innerHTML = str43;
+
+
+            document.getElementById(
+
+                '44').innerHTML = str44;
+
+
+            document.getElementById(
+
+                '45').innerHTML = str45;
+
+
+            document.getElementById(
+
+                '46').innerHTML = str46;
+
+
+            document.getElementById(
+
+                '47').innerHTML = str47;
+
+
+            document.getElementById(
+
+                '48').innerHTML = str48;
+
+
+            document.getElementById(
+
+                '49').innerHTML = str49;
+
+
+            document.getElementById(
+
+                '50').innerHTML = str50;
+
+
+            document.getElementById(
+
+                '51').innerHTML = str51;
+
+
+            document.getElementById(
+
+                '52').innerHTML = str52;
+
+
+            document.getElementById(
+
+                '53').innerHTML = str53;
+
+
+            document.getElementById(
+
+                '54').innerHTML = str54;
+
+
+            document.getElementById(
+
+                '55').innerHTML = str55;
+
+
+            document.getElementById(
+
+                '56').innerHTML = str56;
+
+
+            document.getElementById(
+
+                '57').innerHTML = str57;
+
+
+            document.getElementById(
+
+                '58').innerHTML = str58;
+
+
+            document.getElementById(
+
+                '59').innerHTML = str59;
+
+
+            document.getElementById(
+
+                '60').innerHTML = str60;
+
+
+            document.getElementById(
+
+                '61').innerHTML = str61;
+
+
+            document.getElementById(
+
+                '62').innerHTML = str62;
+
+
+            document.getElementById(
+
+                '63').innerHTML = str63;
+
+
+            document.getElementById(
+
+                '64').innerHTML = str64;
+
+
+            document.getElementById(
+
+                '65').innerHTML = str65;
+
+
+            document.getElementById(
+
+                '66').innerHTML = str66;
+
+
+            document.getElementById(
+
+                '67').innerHTML = str67;
+
+
+            document.getElementById(
+
+                '68').innerHTML = str68;
+
+
+            document.getElementById(
+
+                '69').innerHTML = str69;
+
+
+            document.getElementById(
+
+                '70').innerHTML = str70;
+
+
+            document.getElementById(
+
+                '71').innerHTML = str71;
+
+
+            document.getElementById(
+
+                '72').innerHTML = str72;
+
+
+            document.getElementById(
+
+                '73').innerHTML = str73;
+
+*/
+        };
+
+
+
+
+
